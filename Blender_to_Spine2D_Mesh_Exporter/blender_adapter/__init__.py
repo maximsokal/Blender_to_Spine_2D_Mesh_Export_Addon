2a97cd3c275dee7600097d32936f55225fb25993
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-15 22:12:08 +0300

    Expose temporary mesh and UV unwrap adapters

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
@@ -1,5 +1,10 @@
 """Blender API adapters for the rewritten exporter."""
 
+from .context_state import (
+    BlenderContextError,
+    BlenderContextState,
+    activate_object_for_operator,
+)
 from .evaluated_mesh_reader import (
     EvaluatedMeshReadError,
     EvaluatedMeshSnapshotResult,
@@ -7,12 +12,29 @@ from .evaluated_mesh_reader import (
     read_evaluated_mesh_snapshot,
 )
 from .mesh_reader import MeshReadError, read_source_mesh_snapshot
+from .mesh_writer import MeshWriteError, TemporaryMeshObject, temporary_mesh_object
+from .uv_unwrap import (
+    UvOperatorPlan,
+    UvUnwrapError,
+    build_uv_operator_plan,
+    unwrap_snapshot_uv,
+)
 
 __all__ = [
+    "BlenderContextError",
+    "BlenderContextState",
     "EvaluatedMeshReadError",
     "EvaluatedMeshSnapshotResult",
     "LineageAttributeNames",
     "MeshReadError",
+    "MeshWriteError",
+    "TemporaryMeshObject",
+    "UvOperatorPlan",
+    "UvUnwrapError",
+    "activate_object_for_operator",
+    "build_uv_operator_plan",
     "read_evaluated_mesh_snapshot",
     "read_source_mesh_snapshot",
+    "temporary_mesh_object",
+    "unwrap_snapshot_uv",
 ]
