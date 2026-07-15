3debaaa804781d54bfc4212ee3e6ec63f4ad301a
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-15 22:00:28 +0300

    Expose evaluated mesh reader

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
@@ -1,5 +1,18 @@
 """Blender API adapters for the rewritten exporter."""
 
+from .evaluated_mesh_reader import (
+    EvaluatedMeshReadError,
+    EvaluatedMeshSnapshotResult,
+    LineageAttributeNames,
+    read_evaluated_mesh_snapshot,
+)
 from .mesh_reader import MeshReadError, read_source_mesh_snapshot
 
-__all__ = ["MeshReadError", "read_source_mesh_snapshot"]
+__all__ = [
+    "EvaluatedMeshReadError",
+    "EvaluatedMeshSnapshotResult",
+    "LineageAttributeNames",
+    "MeshReadError",
+    "read_evaluated_mesh_snapshot",
+    "read_source_mesh_snapshot",
+]
