59decd1a6f6e53782c6b1e4c570aeb8a0c29b7e5
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-15 22:40:58 +0300

    Expose A1 attachment projection use case

--- Blender_to_Spine2D_Mesh_Exporter/application/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/application/__init__.py
@@ -1,5 +1,22 @@
 """Application use-case contracts for the rewrite pipeline."""
 
+from .a1_attachment_projection import (
+    A1AttachmentProjectionError,
+    A1AttachmentProjectionResult,
+    A1AttachmentProjectionSettings,
+    A1VertexZBinding,
+    project_triangulated_disk_attachment,
+)
 from .contracts import ExportIssue, ExportRequest, ExportResult, ExportSettings
 
-__all__ = ["ExportIssue", "ExportRequest", "ExportResult", "ExportSettings"]
+__all__ = [
+    "A1AttachmentProjectionError",
+    "A1AttachmentProjectionResult",
+    "A1AttachmentProjectionSettings",
+    "A1VertexZBinding",
+    "ExportIssue",
+    "ExportRequest",
+    "ExportResult",
+    "ExportSettings",
+    "project_triangulated_disk_attachment",
+]
