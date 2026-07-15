4c9155c648842ae6b8d68bcb2499b697c3408fce
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-15 22:24:51 +0300

    Expose transactional bake executor

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
@@ -1,5 +1,17 @@
 """Blender API adapters for the rewritten exporter."""
 
+from .bake_executor import BakeExecutionError, execute_bake_plan
+from .bake_materials import (
+    BakeMaterialError,
+    PreparedBakeMaterials,
+    temporary_bake_materials,
+)
+from .bake_scene_state import (
+    BakeSceneState,
+    BakeSceneStateError,
+    configure_scene_for_bake,
+    preserve_bake_scene_state,
+)
 from .context_state import (
     BlenderContextError,
     BlenderContextState,
@@ -26,6 +38,10 @@ from .uv_unwrap import (
 )
 
 __all__ = [
+    "BakeExecutionError",
+    "BakeMaterialError",
+    "BakeSceneState",
+    "BakeSceneStateError",
     "BlenderContextError",
     "BlenderContextState",
     "EvaluatedMeshReadError",
@@ -34,6 +50,7 @@ __all__ = [
     "MaterialAnalysisError",
     "MeshReadError",
     "MeshWriteError",
+    "PreparedBakeMaterials",
     "TemporaryMeshObject",
     "UvOperatorPlan",
     "UvUnwrapError",
@@ -41,8 +58,12 @@ __all__ = [
     "analyse_material_slot",
     "analyse_object_materials",
     "build_uv_operator_plan",
+    "configure_scene_for_bake",
+    "execute_bake_plan",
+    "preserve_bake_scene_state",
     "read_evaluated_mesh_snapshot",
     "read_source_mesh_snapshot",
+    "temporary_bake_materials",
     "temporary_mesh_object",
     "unwrap_snapshot_uv",
 ]
