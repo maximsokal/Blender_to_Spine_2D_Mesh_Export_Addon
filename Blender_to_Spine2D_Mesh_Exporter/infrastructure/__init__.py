8226ce450bf501f1c24dec5c123301567e510aaf
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-16 00:25:43 +0300

    Export staged UTF-8 text writer

--- Blender_to_Spine2D_Mesh_Exporter/infrastructure/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/infrastructure/__init__.py
@@ -6,10 +6,13 @@ from .atomic_files import (
     AtomicOutputReservation,
     atomic_file_transaction,
 )
+from .staged_text import StagedTextWriteError, write_staged_utf8_text
 
 __all__ = [
     "AtomicFileCommitError",
     "AtomicFileTransaction",
     "AtomicOutputReservation",
+    "StagedTextWriteError",
     "atomic_file_transaction",
+    "write_staged_utf8_text",
 ]
