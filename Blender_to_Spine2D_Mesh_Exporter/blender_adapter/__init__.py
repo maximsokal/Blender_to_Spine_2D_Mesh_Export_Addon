847c157d135f21943f340a1f87cbb41f907420a5
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-16 15:02:41 +0300

    Export graph analysis and bake compositor adapters

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
@@ -18,6 +18,13 @@ from .a1_object_preparation import (
     prepare_a1_object,
 )
 from .a1_single_object_export import export_a1_single_object
+from .bake_compositor import (
+    BakeCompositeError,
+    BakePixelBuffer,
+    compose_bake_passes,
+    read_bake_image_pixels,
+    write_bake_image_pixels,
+)
 from .bake_executor import (
     BakeExecutionError,
     build_bake_execution_result,
@@ -53,6 +60,10 @@ from .material_analyzer import (
 )
 from .mesh_reader import MeshReadError, read_source_mesh_snapshot
 from .mesh_writer import MeshWriteError, TemporaryMeshObject, temporary_mesh_object
+from .shader_graph_analyzer import (
+    MaterialGraphAnalysisError,
+    analyse_material_graph,
+)
 from .uv_unwrap import (
     UvOperatorPlan,
     UvUnwrapError,
@@ -64,8 +75,10 @@ __all__ = [
     "A1MultiObjectPreparationError",
     "A1MultiObjectSource",
     "A1ObjectPreparationError",
+    "BakeCompositeError",
     "BakeExecutionError",
     "BakeMaterialError",
+    "BakePixelBuffer",
     "BakeSceneState",
     "BakeSceneStateError",
     "BlenderContextError",
@@ -74,6 +87,7 @@ __all__ = [
     "EvaluatedMeshSnapshotResult",
     "LineageAttributeNames",
     "MaterialAnalysisError",
+    "MaterialGraphAnalysisError",
     "MeshReadError",
     "MeshWriteError",
     "PreparedA1MultiObject",
@@ -84,10 +98,12 @@ __all__ = [
     "UvOperatorPlan",
     "UvUnwrapError",
     "activate_object_for_operator",
+    "analyse_material_graph",
     "analyse_material_slot",
     "analyse_object_materials",
     "build_bake_execution_result",
     "build_uv_operator_plan",
+    "compose_bake_passes",
     "configure_scene_for_bake",
     "execute_bake_plan",
     "export_a1_mixed_object",
@@ -97,10 +113,12 @@ __all__ = [
     "prepare_a1_multi_object",
     "prepare_a1_object",
     "preserve_bake_scene_state",
+    "read_bake_image_pixels",
     "read_evaluated_mesh_snapshot",
     "read_source_mesh_snapshot",
     "stage_bake_plan_outputs",
     "temporary_bake_materials",
     "temporary_mesh_object",
     "unwrap_snapshot_uv",
+    "write_bake_image_pixels",
 ]
