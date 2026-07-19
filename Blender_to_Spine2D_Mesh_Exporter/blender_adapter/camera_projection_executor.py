b5e6e4134ba514cf42c85e7514b266b7e1fb4f68
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-19 10:50:25 +0300

    Route B4 facade to physical owners

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/camera_projection_executor.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/camera_projection_executor.py
@@ -1,16 +1,17 @@
 """Stable public facade for B4 camera render projection execution."""
 
-from .camera_projection_executor_core import (
+from .camera_projection_error import CameraProjectionExecutionError
+from .camera_projection_output import (
     CameraProjectionStageResult,
     execute_camera_projection_plan,
     stage_camera_projection_outputs,
     stage_camera_projection_outputs_detailed,
 )
 from .camera_projection_state import (
-    CameraProjectionExecutionError,
     configure_scene_for_camera_projection,
     preserve_camera_projection_state,
 )
+
 
 __all__ = [
     "CameraProjectionExecutionError",
