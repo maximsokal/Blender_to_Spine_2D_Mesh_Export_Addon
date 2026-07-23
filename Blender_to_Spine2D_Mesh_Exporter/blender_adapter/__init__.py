c4ee6c0af4f3dedcabefc14bd8cf041c78489c75
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-23 19:42:31 +0300

    Export physical Blender 5.2 execution owners

--- Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/blender_adapter/__init__.py
@@ -1,4 +1,4 @@
-"""Blender API adapters for the rewritten exporter."""
+"""Blender 5.2 API adapters for the rewritten exporter."""
 
 from .a1_mixed_object_export import prepare_a1_mixed_object
 from .a1_mixed_object_output import export_a1_mixed_object
@@ -23,13 +23,7 @@ from .bake_compositor import (
     read_bake_image_pixels,
     write_bake_image_pixels,
 )
-from .bake_executor import (
-    BakeExecutionError,
-    CameraProjectionExecutionError,
-    build_bake_execution_result,
-    execute_bake_plan,
-    stage_bake_plan_outputs,
-)
+from .bake_execution_error import BakeExecutionError
 from .bake_materials import (
     BakeMaterialError,
     PreparedBakeMaterials,
@@ -41,12 +35,14 @@ from .bake_scene_state import (
     configure_scene_for_bake,
     preserve_bake_scene_state,
 )
-from .camera_projection_executor import (
-    configure_scene_for_camera_projection,
+from .camera_projection_error import CameraProjectionExecutionError
+from .camera_projection_output import (
     execute_camera_projection_plan,
-    preserve_camera_projection_state,
-    stage_camera_projection_outputs,
     stage_camera_projection_outputs_detailed,
+)
+from .camera_projection_state import (
+    configure_scene_for_camera_projection,
+    preserve_camera_projection_state,
 )
 from .context_state import (
     BlenderContextError,
@@ -77,15 +73,21 @@ from .scene_bake_capture import (
 from .scene_bake_error import SceneBakeAnalysisError
 from .scene_bake_resources import analyse_object_bake_context
 from .scene_bake_runtime import validate_runtime_scene_context
+from .semantic_bake_output import (
+    build_bake_execution_result,
+    stage_bake_plan_outputs,
+)
 from .shader_capability_audit import audit_material_graph_capabilities
 from .shader_graph_analysis import analyse_material_graph
 from .shader_graph_error import MaterialGraphAnalysisError
+from .texture_executor import execute_bake_plan, stage_texture_plan_outputs
 from .uv_unwrap import (
     UvOperatorPlan,
     UvUnwrapError,
     build_uv_operator_plan,
     unwrap_snapshot_uv,
 )
+
 
 __all__ = [
     "A1MultiObjectPreparationError",
@@ -146,8 +148,8 @@ __all__ = [
     "render_engine_contract",
     "render_engine_contract_from_execution",
     "stage_bake_plan_outputs",
-    "stage_camera_projection_outputs",
     "stage_camera_projection_outputs_detailed",
+    "stage_texture_plan_outputs",
     "temporary_bake_materials",
     "temporary_mesh_object",
     "unwrap_snapshot_uv",
