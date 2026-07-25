2a7a06f440df104cc98357c6165f6810e7f13ea2
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-25 15:30:07 +0300

    Export exclusive operation guard APIs

--- Blender_to_Spine2D_Mesh_Exporter/infrastructure/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/infrastructure/__init__.py
@@ -30,6 +30,13 @@ from .blender_registration import (
     rna_property_cleanup_actions,
     unregister_all_best_effort,
 )
+from .exclusive_operation import (
+    A1_EXPORT_OPERATION_KEY,
+    ExclusiveOperationLease,
+    OperationAlreadyRunningError,
+    active_exclusive_operations,
+    exclusive_operation,
+)
 from .export_diagnostics import (
     ExportDiagnosticsPolicy,
     configure_export_diagnostics,
@@ -50,6 +57,7 @@ from .logging_registry import (
 from .staged_text import StagedTextWriteError, write_staged_utf8_text
 
 __all__ = [
+    "A1_EXPORT_OPERATION_KEY",
     "AtomicCleanupReport",
     "AtomicFileCommitError",
     "AtomicFileTransaction",
@@ -61,22 +69,26 @@ __all__ = [
     "AtomicWorkFileState",
     "AtomicWorkTokenMetadata",
     "DEFAULT_STALE_WORK_FILE_AGE_SECONDS",
+    "ExclusiveOperationLease",
     "ExportDiagnosticsPolicy",
     "ExportEvent",
     "ExportEventDispatcher",
     "ExportEventKind",
     "GLOBAL_EXPORT_EVENTS",
     "ModuleLogLevel",
+    "OperationAlreadyRunningError",
     "RegistrationCleanupAction",
     "RegistrationCleanupError",
     "RegistrationCleanupFailure",
     "RnaPropertyRegistration",
     "StagedTextWriteError",
+    "active_exclusive_operations",
     "assess_atomic_work_file",
     "atomic_file_transaction",
     "class_cleanup_actions",
     "configure_export_diagnostics",
     "discover_python_modules",
+    "exclusive_operation",
     "get_export_diagnostics_policy",
     "merge_module_levels",
     "read_process_start_marker",
