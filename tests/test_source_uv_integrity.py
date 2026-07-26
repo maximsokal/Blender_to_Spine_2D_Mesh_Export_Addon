a4c48b1c61727ac87c1bf1ea7e357629b067a5c4
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-26 16:30:03 +0300

    Cover material UV attribute discrimination

--- tests/test_source_uv_integrity.py
+++ tests/test_source_uv_integrity.py
@@ -7,6 +7,7 @@ from Blender_to_Spine2D_Mesh_Exporter.blender_adapter.source_uv_integrity import
     SourceUvIntegrityError,
     SourceUvMutationError,
     capture_source_uv_fingerprint,
+    material_required_uv_layer_names,
     require_object_mode,
     require_source_uv_unchanged,
     resolve_readable_source_uv_layer_names,
@@ -40,12 +41,37 @@ class _Mesh:
         return id(self)
 
 
+class _NodeTree:
+    def __init__(self, nodes):
+        self.nodes = tuple(nodes)
+
+    def as_pointer(self):
+        return id(self)
+
+
+class _AttributeNode:
+    type = "ATTRIBUTE"
+    node_tree = None
+
+    def __init__(self, attribute_name):
+        self.attribute_name = attribute_name
+
+
+class _UvMapNode:
+    type = "UVMAP"
+    node_tree = None
+
+    def __init__(self, uv_map):
+        self.uv_map = uv_map
+
+
 class _Object:
     type = "MESH"
 
-    def __init__(self, mesh, material_slots=()):
+    def __init__(self, mesh, nodes=()):
         self.data = mesh
-        self.material_slots = tuple(material_slots)
+        material = SimpleNamespace(node_tree=_NodeTree(nodes))
+        self.material_slots = (SimpleNamespace(material=material),) if nodes else ()
 
 
 class _BoundaryMode:
@@ -91,6 +117,26 @@ def test_missing_explicit_boundary_uv_layer_is_blocker():
             obj,
             _settings("EXPLICIT_LAYER", "Missing"),
         )
+
+
+def test_attribute_node_only_requires_real_uv_layer_names():
+    uv_layer = _UvLayer("UVMap", ((0.0, 0.0), (1.0, 0.0), (0.0, 1.0)))
+    obj = _Object(
+        _Mesh((uv_layer,), active=uv_layer),
+        nodes=(
+            _AttributeNode("ColorAttribute"),
+            _AttributeNode("UVMap"),
+        ),
+    )
+
+    assert material_required_uv_layer_names(obj) == ("UVMap",)
+
+
+def test_explicit_uv_map_node_requires_named_layer_even_when_missing():
+    obj = _Object(_Mesh((), active=None), nodes=(_UvMapNode("MissingUV"),))
+
+    with pytest.raises(SourceUvIntegrityError, match="missing required UV"):
+        resolve_readable_source_uv_layer_names(obj, _settings())
 
 
 def test_source_uv_fingerprint_detects_coordinate_mutation():
