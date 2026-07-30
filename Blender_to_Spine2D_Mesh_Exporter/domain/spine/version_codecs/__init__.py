57771f09828445bb5d8164d89ddf6d23143eee61
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-30 22:26:32 +0300

    Export Spine 4.0 codec

--- Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
@@ -6,10 +6,12 @@ from .registry import (
     resolve_spine_json_codec,
     serialize_spine_document,
 )
+from .v40 import Spine40JsonCodec
 from .v41 import Spine41JsonCodec
 from .v42 import Spine42JsonCodec
 
 __all__ = [
+    "Spine40JsonCodec",
     "Spine41JsonCodec",
     "Spine42JsonCodec",
     "SpineJsonCodecContext",
