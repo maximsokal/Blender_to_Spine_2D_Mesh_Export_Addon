1cf12f823bfc867ee3e4539c4f824f57d652f9bb
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-31 09:55:09 +0300

    feat(spine43): expose unified constraints codec

--- Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
@@ -9,11 +9,13 @@ from .registry import (
 from .v40 import Spine40JsonCodec
 from .v41 import Spine41JsonCodec
 from .v42 import Spine42JsonCodec
+from .v43 import Spine43JsonCodec
 
 __all__ = [
     "Spine40JsonCodec",
     "Spine41JsonCodec",
     "Spine42JsonCodec",
+    "Spine43JsonCodec",
     "SpineJsonCodecContext",
     "SpineJsonVersionCodec",
     "registered_spine_json_codecs",
