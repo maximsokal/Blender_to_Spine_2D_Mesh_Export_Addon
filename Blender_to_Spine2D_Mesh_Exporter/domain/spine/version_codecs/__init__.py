09a1b138b27ba21c3871d71f2cff612fb475d881
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-07-31 11:46:24 +0300

    Export Spine 3.8 JSON codec

--- Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
+++ Blender_to_Spine2D_Mesh_Exporter/domain/spine/version_codecs/__init__.py
@@ -6,12 +6,14 @@ from .registry import (
     resolve_spine_json_codec,
     serialize_spine_document,
 )
+from .v38 import Spine38JsonCodec
 from .v40 import Spine40JsonCodec
 from .v41 import Spine41JsonCodec
 from .v42 import Spine42JsonCodec
 from .v43 import Spine43JsonCodec
 
 __all__ = [
+    "Spine38JsonCodec",
     "Spine40JsonCodec",
     "Spine41JsonCodec",
     "Spine42JsonCodec",
