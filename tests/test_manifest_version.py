c16a6ec5b4dcc8291b82fbd3b6f39e591f3432ce
Maxim Sokolenko <33831994+maximsokal@users.noreply.github.com>
2026-08-04 14:47:30 +0300

    Pin manifest contract to release 0.90.0

--- tests/test_manifest_version.py
+++ tests/test_manifest_version.py
@@ -11,7 +11,7 @@ MANIFEST = (
     / "Blender_to_Spine2D_Mesh_Exporter"
     / "blender_manifest.toml"
 )
-EXPECTED_RELEASE_VERSION = "0.81.0"
+EXPECTED_RELEASE_VERSION = "0.90.0"
 
 
 def test_extension_manifest_version_is_current_release() -> None:
